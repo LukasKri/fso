0b7b80701302bf8fc7d5886ae53dba2da1f2fd6d
Lukas Krisikaitis <Lukas.Krisikaitis@ibm.com>
2022-05-08 12:29:27 +0300

    folder structure changes

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,22 @@
+const Hello = ({name, age}) => {
+  return (
+    <div>
+      <p>Hello {name}, you are {age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App
