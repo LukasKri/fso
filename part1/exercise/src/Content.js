17ac5e299fd61cb5662193438c9bf1592b915af4
Lukas Krisikaitis <Lukas.Krisikaitis@ibm.com>
2022-05-08 13:21:12 +0300

    completes step2

--- part1/exercise/src/Content.js
+++ part1/exercise/src/Content.js
@@ -1,17 +1,12 @@
 import React from "react";
+import Part from "./Part";
 
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercises={exercises1}/>
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
     </div>
   );
 };
