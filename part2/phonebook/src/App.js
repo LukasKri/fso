4994821f76e261583de70ca288ef647f331fc418
Lukas Krisikaitis <Lukas.Krisikaitis@ibm.com>
2022-05-14 13:28:30 +0300

    Completes setp4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,23 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchInput, setSearchInput] = useState("");
 
+  const handleSearchInput = (event) => setSearchInput(event.target.value);
   const handleNameChange = (event) => setNewName(event.target.value);
   const handleNumberChange = (event) => setNewNumber(event.target.value);
 
   const resetFormInputs = () => {
     setNewName("");
-    setNewNumber("")
-  }
+    setNewNumber("");
+  };
   const handleFormSubmit = (event) => {
     event.preventDefault();
     const personObject = {
@@ -27,13 +32,18 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
     } else {
       setPersons(persons.concat(personObject));
-      resetFormInputs()
+      resetFormInputs();
     }
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={searchInput} onChange={handleSearchInput} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleFormSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -46,9 +56,17 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>{person.name} {person.number}</div>
-      ))}
+      {persons
+        .filter(
+          (person) =>
+            person.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+            searchInput === ""
+        )
+        .map((person) => (
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
+        ))}
     </div>
   );
 };
