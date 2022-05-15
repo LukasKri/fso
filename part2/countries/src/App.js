e7618a1899a47dc1bd1b16b0007150b6fc09be0c
Lukas Krisikaitis <Lukas.Krisikaitis@ibm.com>
2022-05-15 22:31:05 +0300

    Adds axios.get to countries app

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [foundCountries, setFoundCountries] = useState([]);
+
+  const foundCountriesCondition = countries.filter(({ name: { common } }) =>
+    common.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
+  console.log(foundCountries);
+
+  const handleSearchInput = (e) => {
+    setSearchInput(e.target.value);
+    setFoundCountries(foundCountriesCondition);
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => setCountries(response.data));
+  }, []);
+
   return (
-    <>
-      <h1>Web development curriculum</h1>
-    </>
+    <div>
+      <div>
+        find countries
+        <input value={searchInput} onChange={handleSearchInput} />
+      </div>
+      <div>
+        {foundCountries.length > 10 ? (
+          "Too many matches, specify another filter"
+        ) : foundCountries.length === 1 ? (
+          <h1>one</h1>
+        ) : (
+          foundCountries.map((country) => (
+            <div key={country.name.official}>{country.name.common}</div>
+          ))
+        )}
+      </div>
+    </div>
   );
 };
 
