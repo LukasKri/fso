d5aec5ae047c47a1467df388250f3e4e78f7e831
Lukas Krisikaitis <Lukas.Krisikaitis@ibm.com>
2022-05-19 09:34:54 +0300

    Completes step3

--- part2/countries/src/Country.js
+++ part2/countries/src/Country.js
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Country = ({ foundCountry }) => {
+  const [temp, setTemp] = useState("");
+  const [wind, setWind] = useState("");
+  const [icon, setIcon] = useState("");
+  console.log("🚀 ~ file: Country.js ~ line 8 ~ Country ~ icon", icon);
+
+  const {
+    name: { common },
+    capital,
+    capitalInfo: { latlng },
+    area,
+    languages,
+    flags: { png },
+  } = foundCountry;
+
+  const [lat, lng] = latlng;
+
+  const OPEN_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&units=metric`;
+
+  useEffect(() => {
+    axios.get(OPEN_WEATHER_URL).then((response) => {
+      setTemp(response.data.main.temp);
+      setWind(response.data.wind.speed);
+      setIcon(response.data.weather[0].icon);
+    });
+  }, [OPEN_WEATHER_URL]);
+
   return (
     <div>
-      <h1>{foundCountry.name.common}</h1>
-      <div>capital {foundCountry.capital}</div>
-      <div>area {foundCountry.area}</div>
+      <h1>{common}</h1>
+      <div>capital {capital}</div>
+      <div>area {area}</div>
       <h3>languages:</h3>
       <ul>
-        {Object.values(foundCountry.languages).map((lang) => (
+        {Object.values(languages).map((lang) => (
           <li key={lang}>{lang}</li>
         ))}
       </ul>
-      <img alt="Country flag" src={foundCountry.flags.png} />
+      <img alt="Country flag" src={png} />
+      <h3>Weather in {capital}</h3>
+      <div>temperature {temp} Celsius</div>
+      {icon && (
+        <img
+          alt="Weather status icon"
+          src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+        />
+      )}
+      <div>wind {wind} m/s</div>
     </div>
   );
 };
